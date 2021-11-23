8a8eef82143368b21c4aea1c2d8d8f8fec4349ad
Optinux <tobias.eglseder2@gmail.com>
2021-11-23 20:43:34 +0100

    added more stuff

--- klausur.java
+++ klausur.java
@@ -3,6 +3,8 @@ public class klausur {
     int b;
     int c;
     double d;
+    boolean AnAus;
+    int f;
 
     public klausur(){
         this.a = 0;
@@ -23,13 +25,45 @@ public class klausur {
             }
 
             public void printResult(){
-        System.out.println("Result: " + d);
+                System.out.println("Result a: " + a);
+                System.out.println("Result b: " + b);
+                System.out.println("Result c: " + c);
+                System.out.println("Result d: " + d);
             }
+
+        public double calculate2(int a, int b, int c, boolean AnAus, int f){
+        int temp;
+        for(int i = 0; i < 8; i++){    //broken due to the values always getting set to the same numbers lol
+            temp = a + b;
+            d = temp * c;
+            }
+
+        if(AnAus == true){
+            if (d > f) {
+                System.out.println("Größer als " + f);
+            } else {
+                System.out.println("Kleiner als " + f);
+            }
+        }else{System.out.println("Keine Überprüfung durchgeführt!");}
+
+        return d;
+        }
+
+        public void resetValues(){
+        a = 0;
+        b = 0;
+        c = 0;
+        d = 0;
+        }
 
 
     public static void main(String[] args) {
       klausur k1 = new klausur();
       k1.calculate(40,5,6);
+      k1.printResult();
+      k1.resetValues();
+      k1.printResult();
+      k1.calculate2(4, 5, 3, true, 28);
       k1.printResult();
     }
 }
